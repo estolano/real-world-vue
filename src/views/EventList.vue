9fb7744f10e52756e27be019add6351acd5cf43c
github_estolano <mario.estolano@gmail.com>
2019-02-23 11:31:43 -0300

    Lesson 8 - in the middle

---
+++ src/views/EventList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Events Listing</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event"/>
+  </div>
+</template>
+<script>
+import EventCard from '@/components/EventCard.vue'
+import axios from 'axios'
+
+export default {
+  components: {
+    EventCard
+  },
+  data() {
+    return {
+      events: []
+    }
+  },
+  created() {
+    axios
+      .get('http://localhost:3000/events') // Does a get request
+      .then(response => {
+        console.log(response.data) // For now, logs out the response
+        this.events = response.data
+      })
+      .catch(error => {
+        console.log('There was an error:', error.response) // Logs out the error
+      })
+  }
+}
+</script>
