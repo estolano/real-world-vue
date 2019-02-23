320b3d0b82932e87069512ce2b8224297240262f
github_estolano <mario.estolano@gmail.com>
2019-02-23 14:17:54 -0300

    Lesson 4

--- src/store.js
+++ src/store.js
@@ -5,11 +5,43 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    user: {
-      id: 'abc123',
-      name: 'Adam Jahr'
-    }
+    user: { id: 'abc123', name: 'Adam Jahr' },
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community'
+    ],
+    todos: [
+      { id: 1, text: '...', done: true },
+      { id: 2, text: '...', done: false },
+      { id: 3, text: '...', done: true },
+      { id: 4, text: '...', done: false }
+    ],
+    events: [
+      { id: 1, title: '1...', organizer: '...' },
+      { id: 2, title: '2...', organizer: '...' },
+      { id: 3, title: '3...', organizer: '...' },
+      { id: 4, title: '4...', organizer: '...' }
+    ]
   },
   mutations: {},
-  actions: {}
+  actions: {},
+  getters: {
+    catLength: state => {
+      return state.categories.length
+    },
+    doneTodos: state => {
+      return state.todos.filter(todo => todo.done)
+    },
+    activeTodosCount: state => {
+      return state.todos.filter(todo => !todo.done).length
+    },
+    getEventById: state => id => {
+      return state.events.find(event => event.id === id)
+    }
+  }
 })
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,11 +1,30 @@
 <template>
   <div>
-    <h1>EVentCreate, {{ $store.state.user }}</h1>
+    <h1>Create an Event, {{ user.name }}</h1>
+    <p>This event is created by {{ user.name }}, {{ catLength }} categories.</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
+    <p>Event #1 is {{ getEventById(1) }} .</p>
+    <p></p>
   </div>
 </template>
 
 <script>
-export default {}
-</script>
+import { mapState, mapGetters } from 'vuex'
 
+export default {
+  computed: {
+    catLength() {
+      return this.$store.getters.catLength
+    },
+
+    localComputed() {
+      return 'something'
+    },
+    ...mapGetters(['getEventById']),
+    ...mapState(['categories', 'user']) // <-- using object spread operator
+  }
+}
+</script>
 <style scoped></style>
